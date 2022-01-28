322ec514015811dcf3de26fe14f84909511a5254
7JKaushal <7joshikaushal@gmail.com>
2022-01-28 12:17:42 +0530

    modified Procfile and added dynamic PORT

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.get('*', (req, res) => {
 	res.sendFile(path.join(__dirname + './client/build/index.html'));
 });
 
-app.listen(4000, () => {
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
 	console.log("Listening on port 4000");
 })
